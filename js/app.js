7124844123c0a9a3a7d3d45cb22b2f4ffc7185bd
YirimS <yirimseck@gmail.com>
2021-06-03 20:53:26 -0700

    render chart

--- js/app.js
+++ js/app.js
@@ -53,11 +53,6 @@ function renderRandomProducts() {
   let productOne = selectRandomProductIndex();
   let productTwo = selectRandomProductIndex();
   let productThree = selectRandomProductIndex();
-  // seriously consider using an array.
-  // remember:  how do you know if an array inculdes something? maybe google
-  // while (productOne === productTwo) {
-  //   productTwo = selectRandomProductIndex();
-  // }
 
 
   imageOne.src = allProduct[productOne].src;
@@ -90,6 +85,7 @@ function handleProductClick(event) {
   renderRandomProducts();
 
   if (clicks === clicksAllowed) {
+    renderchart();
     myContainer.removeEventListener('click', handleProductClick);
   }
 }
@@ -109,44 +105,54 @@ function handleButtonClick(event) { //eslint-disable-line
   }
 }
 
+function renderchart() {
+  let productName = [];
+  let productViews = [];
+  let productClicks = [];
+  for (let i = 0; i < allProduct.length; i++) {
+    productName.push(allProduct[i].name);
+    productViews.push(allProduct[i].views);
+    productClicks.push(allProduct[i].clicks);
+  }
 
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of views',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: productName,
+      datasets: [{
+        label: 'views',
+        data: productViews,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
-
+  });
+}
 
 
 
